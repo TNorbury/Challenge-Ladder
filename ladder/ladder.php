34cf49a1b014b87029e072333b67ee48e5081c4d
Tyler Norbury <tylernorbury96@gmail.com>
2017-10-20 18:43:23 +0000

    Changed the navbar so that it is in its own file and will be included where necessary

--- ladder/ladder.php
+++ ladder/ladder.php
@@ -6,25 +6,10 @@
    </head>
 
    <body>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light border border-dark justify-content-between">
-         <ul class="navbar-nav">
-            <li class="nav-item active">
-               <a class="navbar-brand" href="#">The Ladder</a>
-            </li>
-            <li class="nav-item active">
-               <a class="nav-link" href="#">My Challenges</a>
-            </li>
-            <li class="nav-item active">
-               <a class="nav-link" href="#">Games</a>
-            </li>
-         </ul>
-         <ul class="navbar-nav" align="right">
-            <li class="nav-item active">
-               <a class="nav-link" href="/login.html">Log Out</a>
-            </li>
-         </ul>
-      </nav>
-
+      <?php
+         // This will include the navbar
+         include($_SERVER['DOCUMENT_ROOT'].'/navbar/navbar.html');
+      ?>
 
       <div class="container mt-5">
          <div class="row justify-content-center">
