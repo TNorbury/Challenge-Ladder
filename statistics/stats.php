9a5855dbfc3a8d2e355452115afc40d13293d61d
Tyler Norbury <tylernorbury96@gmail.com>
2017-10-27 18:24:49 +0000

    All of the php pages will now start session upon being loaded

--- statistics/stats.php
+++ statistics/stats.php
@@ -1,3 +1,6 @@
+<?php
+   session_start();
+?>
 <html>
    <head>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
