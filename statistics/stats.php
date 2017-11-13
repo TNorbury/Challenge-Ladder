4869e3c4ae27bb1108ff61e18f979e59130a4a3e
Tyler Norbury <tylernorbury96@gmail.com>
2017-11-13 17:44:59 +0000

    The statistics page now displays all players along with their stats

--- statistics/stats.php
+++ statistics/stats.php
@@ -13,9 +13,78 @@
 
    <body>
       <?php
-            // This will include the navbar
+         // This will include the navbar
          include($_SERVER['DOCUMENT_ROOT'].'/navbar/navbar.html');
+
+         // Get the php file that connects to the database
+         require($_SERVER['DOCUMENT_ROOT'].'/dbConnect/dbConnect.php');
+
+         // Get all the players and their stats from the database
+         $result = $connection->query("
+            select name, match_win_pct, game_win_pct, avg_win_margin, avg_lose_margin 
+            from (
+               select p.name, p.rank,
+               coalesce( (select cast(count(*) as float(2)) from match_view as m
+                  where m.winner = p.username)
+                  /
+                  (select cast(count(*) as float(2)) from match_view as m
+                  where m.winner = p.username or m.loser = p.username), 0.0)
+                  as match_win_pct,
+               coalesce( (select cast(count(*) as float(2)) from game as g
+                  where g.winner = p.username)
+                  /
+                  (select cast(count(*) as float(2)) from game as g
+                  where g.winner = p.username or g.loser = p.username), 0.0)
+                  as game_win_pct,
+               coalesce( (select avg(winner_score - loser_score) from game as g
+                  where p.username=g.winner), 0.0) as avg_win_margin,
+               coalesce( (select avg(winner_score - loser_score) from game as g
+                  where p.username=g.loser), 0.0)  as avg_lose_margin
+               from player as p
+               where exists (select * from match_view as m where p.username = m.winner or
+                  p.username=m.loser)
+               union
+               select name, rank, 0.0, 0.0, 0.0, 0.0 from player
+               where not exists (select * from match_view where username = winner or 
+                  username = loser)
+            ) as unordered_stats
+            order by rank ASC
+         ");
+
       ?>
-      This page will display all the people on the ladder, along with various stats (# of games won/lost, win %, etc.) for all of the players.
+
+      <div class="container mt-5">
+         <div class="row justify-content-center">
+            <div class="col-8">
+               <table class="table table-hover">
+                  <thead class="thead-inverse">
+                     <tr>
+                        <th>Name</th>
+                        <th>Match Win %</th>
+                        <th>Game Win %</th>
+                        <th>Avg. Win Margin</th>
+                        <th>Avg. Loss Margin</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php
+                        foreach($result->fetchAll() as $resultRow)
+                        {
+                           echo "
+                              <tr class='table-light'>
+                                 <td>$resultRow[name]</td> 
+                                 <td align='center'>".(round($resultRow[match_win_pct], 2)*100)."%</td> 
+                                 <td align='center'>".(round($resultRow[game_win_pct], 2)*100)."%</td> 
+                                 <td align='center'>".round($resultRow[avg_win_margin], 2)."</td> 
+                                 <td align='center'>".round($resultRow[avg_lose_margin], 2)."</td> 
+                              </tr>";
+                        }
+                     ?>
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      </div>
+
    </body>
 </html>
