6a4bfd323e2bb06ded5be1d676208a16333afef5
Tyler Norbury <tylernorbury96@gmail.com>
2017-11-17 01:27:15 +0000

    The challenge page now displays tables results for challenges recieved and challenges issued. The user can now accept or decline challenges against them

--- challenges/challenges.php
+++ challenges/challenges.php
@@ -13,9 +13,93 @@
 
    <body>
       <?php
-            // This will include the navbar
+         // This will include the navbar
          include($_SERVER['DOCUMENT_ROOT'].'/navbar/navbar.html');
+
+         // Get the php file that connects to the database
+         require($_SERVER['DOCUMENT_ROOT'].'/dbConnect/dbConnect.php');
+
+         // Prepare and execute the query to get all the challenges made 
+         // against the user
+         $othersChallengesQuery = $connection->prepare("
+            select challenger, scheduled, name from challenge
+               join player on challenger = username
+            where challengee = :username
+         ");
+         $othersChallengesQuery->execute(array(':username'=>$_SESSION[username]));
+
+         // Prepare and execute the query to get all the challenges the user has made
+         $myChallengesQuery = $connection->prepare("
+            select challengee, scheduled, name from challenge
+               join player on challengee = username
+            where challenger = :username
+         ");
+         $myChallengesQuery->execute(array(':username'=>$_SESSION[username]));
+
+
       ?>
-      All the challenges that have been made against the user, and those that the user has made, will appear here
+      
+      <div class="container mt-5">
+         <!-- This will have all the challenges made against the user  -->
+         <h5 class="row justify-content-center">Challenges Issued Against Me</h5>
+         <div class="row justify-content-center">
+            <div class="col-6">
+               <table class="table border border-dark">
+                  <thead class="thead-inverse">
+                     <tr>
+                        <th>Player</th>
+                        <th style="text-align:center;">Challenge Date</th>
+                        <th></th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php
+                        foreach ($othersChallengesQuery->fetchAll() as $row) {
+                           echo "
+                              <tr class='table-light'>
+                                 <form action='http://dhansen.cs.georgefox.edu/~dhansen/Classes/ClientServer/Protected/Examples/echoForm.php' method='post'>
+                                    <td>$row[name]</td>
+                                    <td align='right'><input style='border:none;' name='challengeDate' value='$row[scheduled]' readonly></input></td>
+                                    <input type='hidden' name='challengeeUsername' value='$row[challenger]' readonly></input>
+                                    <td align='right'>
+                                       <input class='btn btn-primary btn-sm' type='submit' name='acceptChallenge' value='Accept'></input>
+                                       <input class='btn btn-primary btn-sm' type='submit' name='rejectChallenge' value='Reject'></input>
+                                 </form>
+                              </tr>
+                          ";
+                        }
+                     ?>
+                  </tbody>
+               </table>
+            </div>
+         </div>
+
+         <!-- This will have all the challenges the user has made  -->
+         <h5 class="row justify-content-center">Challenges Issued Against Others</h5>
+         <div class="row justify-content-center">
+            <div class="col-6">
+               <table class="table border border-dark">
+                  <thead class="thead-inverse">
+                     <tr>
+                        <th>Player</th>
+                        <th style="text-align: right">Challenge Date</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php
+                        foreach ($myChallengesQuery->fetchAll() as $row) {
+                           echo "
+                              <tr class='table-light'>
+                                 <td>$row[name]</td>
+                                 <td align='right'>$row[scheduled]</td>
+                              </tr>";
+                        }
+                     ?>
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      </div>
+
    </body>
 </html>
