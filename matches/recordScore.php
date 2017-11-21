e9ec151d249b3ca6050dc0ea07c503e5e1bff922
Tyler Norbury <tylernorbury96@gmail.com>
2017-11-21 00:51:29 +0000

    Record Score is now done Players can now report the scores from a match and have them stored in the database along with being moved up and down the ladder, if necessary

--- matches/recordScore.php
+++ matches/recordScore.php
@@ -19,10 +19,6 @@
    
    // Determine if the challenger is the opponent
    $opponentIsChallenger = $challenger != $_SESSION[username];
-   $winner = "";
-   $winnerScore = 0;
-   $loser = "";
-   $loserScore = 0;
 
    // Go through all the game results and record the results into the database
    for ($gameNum = 1; $gameNum <= 5; $gameNum++) {
@@ -34,7 +30,7 @@
 
       $query = $connection->prepare("
          insert into game (winner, loser, played, number, winner_score, loser_score)
-         values(:winner, :loser, now(), :gameNum, :winnerScore, :loserScore)
+         values(:winner, :loser, CURRENT_DATE, :gameNum, :winnerScore, :loserScore)
       ");
 
 
@@ -59,17 +55,17 @@
          && ((int)$_POST["opponentScore".$gameNum] > (int)$_POST["playerScore".$gameNum]))
       {
          $winner = $challengee;
-         $winnerScore = (int)$_POST["playerScore".$gameNum];
+         $winnerScore = (int)$_POST["opponentScore".$gameNum];
          $loser = $challenger;
-         $loserScore = (int)$_POST["opponentScore".$gameNum];
+         $loserScore = (int)$_POST["playerScore".$gameNum];
       }
       else if ((!$opponentIsChallenger) 
          && ((int)$_POST["opponentScore".$gameNum] < (int)$_POST["playerScore".$gameNum]))
       {
          $winner = $challenger;
-         $winnerScore = (int)$_POST["opponentScore".$gameNum];
+         $winnerScore = (int)$_POST["playerScore".$gameNum];
          $loser = $challengee;
-         $loserScore = (int)$_POST["playerScore".$gameNum];
+         $loserScore = (int)$_POST["opponentScore".$gameNum];
       }
 
       // Now execute the query
@@ -77,8 +73,87 @@
          ':loser'=>$loser, ':loserScore'=>$loserScore, ':gameNum'=>$gameNum));
    }
    
-
    // Move the winner and loser to their appropriate place on the database
+   // Determine if the match winner was the challengee or challenger.
+   if (($opponentIsChallenger && $_POST[matchWinner] == "Opponent") 
+      || (!$opponentIsChallenger && $_POST[matchWinner] == "Player")) 
+   {
+      // Challenger is the winner. We need to readjust the ladder.
+      
+      // Get challenger and challengee's current rank
+      $query = $connection->prepare("
+         select username, rank from player
+         where username = :challenger or username = :challengee
+      ");
+      $query->execute(array(':challenger'=>$challenger, ':challengee'=>$challengee));
+
+      // Iterate through the results of the query (there should only be two) 
+      // and get the rank of the challenger and challengee
+      foreach($query->fetchAll() as $row) {
+         if ($row[username] == $challenger) {
+            $challengerRank = $row[rank];
+         }
+         else {
+            $challengeeRank = $row[rank];
+         }
+      }
+      
+      // Set the challenger's rank to 0, so that the other ranks can be 
+      // adjusted without causing any unique value violations
+      $query = $connection->prepare("
+         update player
+         set rank = 0
+         where username = :challenger
+      ");
+      $query->execute(array(':challenger'=>$challenger));
+      // Get all the players who need their rank update from the database
+      $query = $connection->prepare("
+         select username, rank from player
+         where rank >= :challengeeRank and rank < :challengerRank
+         order by rank DESC
+      ");
+      $query->execute(array(':challengeeRank'=>$challengeeRank, 
+         ':challengerRank'=>$challengerRank));
+
+      // Go through all these people and update their rank in the database
+      foreach($query->fetchAll() as $row)
+      {
+         $newRank = (int)$row[rank] + 1;
+         echo $row[username]."--".$newRank."<br/>";
+         
+         $query = $connection->prepare("
+            update player
+            set rank = :newRank
+            where username = :username
+         ");
+         $query->execute(array(':newRank'=>$newRank, ':username'=>$row[username]));
+      
+      print_r($query->errorInfo());
+      echo "<br/>";
+      }
+
+      // Set challenger's rank equal to challengee's (old) rank
+      $query = $connection->prepare("
+         update player
+         set rank = :challengeeRank
+         where username = :challenger
+      ");
+      $query->execute(array(':challengeeRank'=>$challengeeRank, ':challenger'=>$challenger));
+      echo "<br/>";
+   }
+   else if (($opponentIsChallenger && $_POST[matchWinner] == "Player") 
+      || (!$opponentIsChallenger && $_POST[matchWinner] == "Opponent"))
+   {
+      // Challengee is the winner. There won't be any chagnes made to the ladder.
+   }
 
    // Delete the challenge that was just completed from the database
+   $query = $connection->prepare("
+      delete from challenge
+      where challenger = :challenger and challengee = :challengee
+   ");
+   $query->execute(array(':challengee'=>$challengee, ':challenger'=>$challenger));
+
+   // Redirect the user to the ladder now that their match is over
+   header("Location: /ladder/ladder.php");
 ?>
